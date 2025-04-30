8fae841addd154efdf427c638c7df5d4b2c33091
juliotupacgabino <75981237@continental.edu.pe>
2025-04-30 11:36:08 -0500

    Se implementó cálculo de raíces reales

--- src/logica/ecuacion_segundo_grado.py
+++ src/logica/ecuacion_segundo_grado.py
@@ -1,2 +1,56 @@
+import math
 class EcuacionSegundoGrado:
-    pass
+    def __init__(self, a=0, b=0, c=0):
+        self._a = a
+        self._b = b
+        self._c = c
+
+    @property
+    def a(self):
+        """Getter para a"""
+        return self._a
+
+    @a.setter
+    def a(self, valor):
+        """Setter para a"""
+        if isinstance(valor, (int, float)):
+            self._a = valor
+        else:
+            raise ValueError("a debe ser un número")
+
+    @property
+    def b(self):
+        """Getter para b"""
+        return self._b
+
+    @b.setter
+    def b(self, valor):
+        """Setter para b"""
+        if isinstance(valor, (int, float)):
+            self._b = valor
+        else:
+            raise ValueError("b debe ser un número")
+
+    @property
+    def c(self):
+        """Getter para c"""
+        return self._c
+
+    @c.setter
+    def c(self, valor):
+        """Setter para c"""
+        if isinstance(valor, (int, float)):
+            self._c = valor
+        else:
+            raise ValueError("c debe ser un número")
+
+    def solucionESG(self):
+        return None, None
+
+
+    def solucionESG(self):
+        d = math.pow(self.b, 2) - 4 * self.a * self.c
+        if d >= 0:
+            r1 = (-self.b + math.sqrt(d)) / (2 * self.a)
+            r2 = (-self.b - math.sqrt(d)) / (2 * self.a)
+            return r1, r2
--- tests/test_ecuacion_segundo_grado.py
+++ tests/test_ecuacion_segundo_grado.py
@@ -1,4 +1,18 @@
 import unittest
+from src.logica.ecuacion_segundo_grado import EcuacionSegundoGrado
+
 
 class TestEcuacionSegundoGrado(unittest.TestCase):
-    pass
+    def test_solucionESG_parametrosNumericos_raicesReales(self):
+        # Arrange
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+        ecuacionSegundoGrado.a = 3
+        ecuacionSegundoGrado.b = -5
+        ecuacionSegundoGrado.c = 1
+        RaizEsperada1 = 1.43
+        RaizEsperada2 = 0.23
+        # Do
+        RaizActual1, RaizActual2 = ecuacionSegundoGrado.solucionESG()
+        # Assert
+        self.assertAlmostEqual(RaizEsperada1, RaizActual1, 2)
+        self.assertAlmostEqual(RaizEsperada2, RaizActual2, 2)
