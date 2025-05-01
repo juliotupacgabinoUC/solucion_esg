57e16d3bc383ef88e9c9f440c3d1c6cddf41e68b
juliotupacgabino <75981237@continental.edu.pe>
2025-05-01 10:32:01 -0500

    Se implementó app.py

--- src/vista/app.py
+++ src/vista/app.py
@@ -1,2 +1,13 @@
+from src.logica.ecuacion_segundo_grado import EcuacionSegundoGrado
 if __name__ == "__main__":
-    pass
+    ecuacionSegundoGrado = EcuacionSegundoGrado()
+
+    print("Solución: ax^2 + bx + b");
+    ecuacionSegundoGrado.a = float(input("Parámetro a: "))
+    ecuacionSegundoGrado.b = float(input("Parámetro b: "))
+    ecuacionSegundoGrado.c = float(input("Parámetro c: "))
+    raiz1, raiz2 = ecuacionSegundoGrado.solucionESG()
+    print(f"{ecuacionSegundoGrado.a}x^2 +" \
+                f"{ecuacionSegundoGrado.b}x +" \
+                f"{ecuacionSegundoGrado.b} --> " + \
+                f"raiz1= {raiz1} Raiz2={raiz2}")
