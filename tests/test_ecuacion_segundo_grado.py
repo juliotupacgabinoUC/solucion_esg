1b852677927b9e43b34386683d50554aa7d708c2
juliotupacgabino <75981237@continental.edu.pe>
2025-05-01 10:28:41 -0500

    Se validó el tipo de datos de los parámetros

--- tests/test_ecuacion_segundo_grado.py
+++ tests/test_ecuacion_segundo_grado.py
@@ -89,3 +89,9 @@ class TestEcuacionSegundoGrado(unittest.TestCase):
             self.assertAlmostEqual(raiz_esperada1.imag, raiz_actual1.imag, 2)
             self.assertAlmostEqual(raiz_esperada2.real, raiz_actual2.real, 2)
             self.assertAlmostEqual(raiz_esperada2.imag, raiz_actual2.imag, 2)
+
+    def test_solucionESG_parametrosNoNumericos_lanzaException(self):
+        ecuacionSegundoGrado = EcuacionSegundoGrado()
+
+        with self.assertRaises(ValueError):
+            ecuacionSegundoGrado.a = "a"
